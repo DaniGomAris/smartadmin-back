015df6b372ffd73a6510b9686a0b233a09008ec4
DaniGomAris <danielgomezaristizabal2020@gmail.com>
2025-09-08 16:40:59 -0500

    change .env

--- app/config/firebase.py
+++ app/config/firebase.py
@@ -7,13 +7,12 @@ from firebase_admin import credentials, firestore
 load_dotenv()
 deployment = os.getenv("NODE_ENV")
 
-if os.getenv("NODE_ENV") == "production":
-    firebase_key_path = os.getenv("FIREBASE_KEY_PRODUCTION")
+if deployment == "production":
+    firebase_key_path = os.getenv("FIREBASE_KEY_PATH_PROD")
+elif deployment == "development":
+    firebase_key_path = os.getenv("FIREBASE_KEY_PATH_DEV")
 else:
-    if os.getenv("NODE_ENV") == "development":
-        firebase_key_path = os.getenv("FIREBASE_KEY_DEVELOPMENT")
-    else:
-        raise ValueError("NODE_ENV must be set to 'development' or 'production'")
+    raise ValueError("NODE_ENV must be set to 'development' or 'production'")
 
 cred = credentials.Certificate(firebase_key_path)
 firebase_admin.initialize_app(cred)
