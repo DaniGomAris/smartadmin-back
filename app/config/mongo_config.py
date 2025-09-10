6384c255d30d25c7b23f5e2ea28b318c43fa1d64
DaniGomAris <danielgomezaristizabal2020@gmail.com>
2025-09-09 22:30:11 -0500

    .env change

--- app/config/mongo_config.py
+++ app/config/mongo_config.py
@@ -1,12 +1,5 @@
-# app/config/mongo_config.py
 from pymongo import MongoClient
 from app.config.app_config import Config
 
 client = MongoClient(Config.MONGO_URI)
 db = client[Config.DB_NAME]
-
-try:
-    client.admin.command("ping")
-    print(f"✅ Conectado a MongoDB")
-except Exception as e:
-    print("❌ Error al conectar con MongoDB:", e)
